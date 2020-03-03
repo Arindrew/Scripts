1d06fa19c0674c21c7972ca6547e8b6982abd3ec
Andrew Keller <andrew@thekellers.email>
2020-03-03 07:40:51 -0600

    Update _TEMPLATE_.bat

--- Batch/_TEMPLATE_.bat
+++ Batch/_TEMPLATE_.bat
@@ -1,16 +1,16 @@
 @echo off
 
 @if not "%OS" == "Windows_NT" goto :EXIT
-@if "%1" == "" (set INFO=echo && set ScriptExit=1) else (set INFO=REM && set ScriptExit =0)
+@if "%1" == "" (set INFO=echo && set ScriptExit=1) else (set INFO=REM && set ScriptExit=0)
 
 :EXIT
 
 %INFO% ********************************
-%INFO% Script:
-%INFO% Creation Date:
-%INFO% Last Modified:
+%INFO% Script: 
+%INFO% Creation Date: 
+%INFO% Last Modified: 
 %INFO% Author: Andrew S. Keller
-%INFO% E-mail: arindrew@runbox.com
+%INFO% E-mail: andrew@pm.me
 %INFO% ********************************
 %INFO% Description:
 %INFO% 
