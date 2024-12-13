43da6d9e75af50eddd6e0637aac878604bdf2f0e
Andrew Keller <github@akme.cc>
2024-12-13 10:57:54 -0600

    Update Launcher.sh

--- BASH/Hopper/Launcher.sh
+++ BASH/Hopper/Launcher.sh
@@ -24,7 +24,7 @@ Directory=/mnt/NAS/InputData/HotFolders/Master_Hopper
 #Create destination directory for log creation
 today=/mnt/NAS/DalimUsers/ADMIN/log/Hopper/$(date -I)
 if [[ ! -d $today ]]; then
-    mkdir -p $today
+    mkdir -p "$today"
 fi
 
 #NGA_Hopper scripts can use 3 arguments:
@@ -33,15 +33,15 @@ fi
 #$3 Time Delay between sending files
 
 ((
-cd $Scripts
+cd "$Scripts" || exit
 #for S in $( ls NGA.Hopper_*.sh ); do
 for S in NGA.Hopper_*.sh; do 
-  Workflow=$( ls $S | cut -d\_ -f2-99 | rev | cut -b 4-99 | rev )
+  Workflow=$( find "$S" | cut -d_ -f2-99 | rev | cut -b 4-99 | rev )
   if [[ -d "$Directory/$Workflow" ]]; then
     echo -e "${GREEN}$(date "+%D %H:%M:%S")${NC}"
     echo "### Checking for scripts, found these scripts to start:"
-    echo "$(find "$Scripts" -name "NGA.Hopper*.sh" | awk -F / '{print $NF}')"
-    if [[ "$(ps -ef | grep $S | egrep -cv 'grep | tail -f')" -ge "1" ]]; then
+    find "$Scripts" -name "NGA.Hopper*.sh" | awk -F / '{print $NF}'
+    if [[ "$(pgrep -c "$S")" -ge "1" ]]; then
       echo -e "The Script $S is running, will not start script, will check next cycle\n"
     else
       #Run custom sub routines for Conversions and NOAA, all other scripts treated the same
@@ -49,7 +49,7 @@ for S in NGA.Hopper_*.sh; do
       #SubDir : subdirectory in the above folder
       if [[ "$Workflow" = "Conversions" ]]; then
         #need to add special routine to list directorys in  $Directory/Conversions, treat those as workflows
-        for SubDir in $(find $Directory/$Workflow  -maxdepth 1 -type d | awk -F / '{print $NF}'); do
+        for SubDir in $(find $Directory/"$Workflow" -maxdepth 1 -type d | awk -F / '{print $NF}'); do
           echo -e ""
           echo -e "Calling SubDir sub-routine..."
           echo -e "  Script: $S"  
@@ -58,20 +58,20 @@ for S in NGA.Hopper_*.sh; do
           if [[ "$Directory/$Workflow/$SubDir" = "/mnt/NAS/InputData/HotFolders/Master_Hopper/Conversions/Conversions" ]]; then
             echo "  NOT calling Script for root of $Workflow folder"
           else
-            /bin/bash $Scripts/$S $SubDir $Directory/$Workflow/$SubDir & 
+            /bin/bash "$Scripts"/"$S" "$SubDir" "$Directory"/"$Workflow"/"$SubDir" & 
           fi
         done  
-        elif [[ "$(echo $Workflow | grep -ci NOAA)" -ge "1" ]]; then
+        elif [[ "$(echo "$Workflow" | grep -ci NOAA)" -ge "1" ]]; then
           # Sub routine for NOAA scripts, currently runs only on Saturday, every cron time, need to runs 1 time only on Saturday 00:00
           if [[ "$(date +"%A")" = "Saturday" ]]; then
             echo -e "\n Calling NOAA Scripts on $(date +%A) : $Scripts$S : $Workflow : $Directory/$Workflow"
-            /bin/bash $Scripts/$S $Workflow $Directory/$Workflow & 
+            /bin/bash "$Scripts"/"$S" "$Workflow" "$Directory"/"$Workflow" &
           else
             echo -e "It's not Saturday, not calling $S \n"
           fi
         else
           echo -e "\n### Calling: \n  Script: $Scripts/$S \n  Workflow: $Workflow \n  Directory: $Directory/$Workflow"
-          /bin/bash $Scripts/$S $Workflow $Directory/$Workflow & 
+          /bin/bash "$Scripts"/"$S" "$Workflow" "$Directory"/"$Workflow" &
         fi
       fi
     else
