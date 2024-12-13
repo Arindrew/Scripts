c5a08e583995da0e418a509684899594547fd524
Andrew Keller <github@akme.cc>
2024-12-13 11:22:49 -0600

    Update findOldPDFs.sh

--- BASH/findOldPDFs.sh
+++ BASH/findOldPDFs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 find /mnt/NAS/ctptransfer -type d -path /mnt/NAS/ctptransfer/.snapshot -prune -o \
--name *.pdf ! -name *geo.* -mtime +4015 -exec ls -og --time-style=iso {} \; | cut -d" " -f6 | sort
+-name "*.pdf" ! -name "*geo.*" -mtime +4015 -exec ls -og --time-style=iso {} \; | cut -d" " -f6 | sort
