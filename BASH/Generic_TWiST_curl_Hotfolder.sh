82cc939f30937e8b9e3d87c99d2f5611eeb228db
Andrew Keller <github@akme.cc>
2024-12-13 10:32:17 -0600

    Update Generic_TWiST_curl_Hotfolder.sh

--- BASH/Generic_TWiST_curl_Hotfolder.sh
+++ BASH/Generic_TWiST_curl_Hotfolder.sh
@@ -1,59 +1,34 @@
 #!/bin/bash
 
-# The workflow you want this script to send files to
-Workflow=13_Automate_QRCode
+#Define colors for stdout
+RED='\033[0;31m'
+LRED='\033[1;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No color
 
-# The location of the files you want this script to look for files in
-Folder=/mnt/NAS/InputData/18k_PDFs/NotinSpreadsheet/Finished_INPUT_ALREADY
+# The Workflow you want this script to send files to
+Workflow=12_Automate_noQRCode
 
-# Delay in seconds, betwee transmissions of a file to the server
-Delay=30
+#The location of the files you want this script to look for file in
+Folder=/mnt/NAS/InputData/002_CPENs/2024-12-04_41
 
-# Servers to send files to, 1 server per line, by case-sensitive hostname
-Servers=(
-server1
-server2
-server3
-server4
-server5
-server6
-server7
-server8
-)
+#Delay is in seconds. This is wait time between transmissions.
+Delay=60
 
+while read -r line; do 
+  echo "$(date "+%D %H:%M:%S") Sending: $(echo $line | rev | cut -d/ -f1 | rev)"
+  curl -s -S http://ndwsstwslpdal08:8080/twist/twistUpload -F name=$Workflow -F file=@$line
+  mv "$line" $Folder/Processed/
+  for seconds in $(eval "echo {$Delay..01}"); do 
+    sleep 1
+    echo -en "${GREEN}.${NC}";
+  done
+echo ""
+done < <(find $Folder -maxdepth 1 -type f -name "*.pdf")
 
-
-# Array of files to send to servers
-Files=$(find $folder -maxdepth 1 -type f)
-
-# These are the Server File Index Number count in the Array
-ServerNumber=0
-FileNumber=0
-
-# Function to send filse, this is set to curl as it is easiest. SCP will require the creation of SSH keys
-
-function Transfer() {
-  echo "$date "+%D %H:%M:%S") Calling Transfer Function"
-  echo "    File: $F"
-  echo "    Workflow: $Workflow"
-  echo "    Server: $S"
-  curl -s -S http://server1:8080/twist/twistUpload?server=$S -F name=$Workflow -F file=@$F
-  #rm -rf $F
-  echo "    Waiting $Delay second(s) to send next file"
-  sleep $Delay
-}
-
-for F in ${Files[*]}; do
-  S=${Servers[$ServerNumber]}
-  Transfer
-  echo "$(date "+%D %H:%M:%S") Completed Transfer Function
-  echo ""
-  if [[ "$ServerNumber" == "$(( ${#Servers[@]} -1 ))" ]]; then
-    # $ServerNumber is less than ${#Servers[@]} - resetting back to zero
-    ServerNumber=0
-    FileNumber=$(( $FileNumber + 1 ))
-  else
-    ServerNumber=$(( $ServerNumber + 1 ))
-    FileNumber=$(( $FileNumber + 1 ))
-  fi
-done
+# How to remove layered_attributed from each file
+########
+#  while read -r file; do 
+#    mv $file ${file:0:16}.pdf
+#  done < <(find . -maxdepth 1 -type f -name "*layered_attributed.pdf")
+#######
