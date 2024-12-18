6c7b9a069eb758f66c23ab67ffb6a3d8bad71d7a
Andrew Keller <github@akme.cc>
2024-12-18 09:23:08 -0600

    Update ESCleanup.sh

--- BASH/ES.Scripts/ESCleanup.sh
+++ BASH/ES.Scripts/ESCleanup.sh
@@ -13,26 +13,30 @@ fi
 # Amount of time to wait (in minutes) before moving the files
 TimeToWait=30
 
+# Touch the $DTODir to ensure that it is not older than $TimeToWait
+touch $DTODir
+
 # Search through the DTO Directory looking for folders that
 # are older than the TimeToWait variable and deleting them
-touch $DTODir
-find $DTODir -type d -mmin +$TimeToWait | while IFS= read -r file; do
+while read -r file; do
     if [ -d "${file}" ]; then
         printf '%s\n' "Folder that is older than 30 minutes: ${file}" | tee -a "$today"/DTOCleanup.log
         rmdir -v "${file}" | tee -a "$today"/DTOCleanup.log
     else
         printf '%s\n' "No files found"
     fi
-done
+done < <(find $DTODir -type d -mmin +$TimeToWait)
+
+# Touch the $AssemblyDir to ensure that it is not older than $TimeToWait
+touch $AssemblyDir
 
 # Search through the Assembly Directory looking for folders that
 # are older than the TimeToWait variable and deleting them
-touch $AssemblyDir
-find $AssemblyDir -type d -mmin +$TimeToWait | while IFS= read -r file; do
+while read -r file; do
     if [ -d "${file}" ]; then
         printf '%s\n' "Folder that is older than 30 minutes: ${file}" | tee -a "$today"/AssemblyCleanup.log
         rmdir -v "${file}" | tee -a "$today"/AssemblyCleanup.log
     else
         printf '%s\n' "No files found"
     fi
-done
+done < <(find $AssemblyDir -type d -mmin +$TimeToWait)
